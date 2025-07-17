84bdabfeba500e267efe54ea862fe876ca46c679
ladytriana <ladytriana2005@gmail.com>
2025-07-17 10:16:24 +0700

    berubah

--- src/pages/hubungi.vue
+++ src/pages/hubungi.vue
@@ -1,40 +1,10 @@
 <template>
-<<<<<<< HEAD
-  <div class="hubungi-container fade-in-up">
-    <div class="hubungi-text">
-      <h1>Hubungi Kami</h1>
-      <p style="font-weight: 100 !important;">
-        Punya pertanyaan soal program UMKM atau ingin tahu estimasi biaya<br />
-        pelatihan atau fasilitas di Rumah BUMN? Yuk, langsung aja hubungi kami<br />
-        lewat email atau telepon.
-      </p>
-      <ul class="contact-list">
-        <li style="font-weight: 100 !important; display: flex; align-items: center; gap: 10px;">
-          <span class="icon" style="font-size: 1.5rem; color: #0E3B4F;">📞</span>
-          <span>0812345678910</span>
-        </li>
-        <li style="font-weight: 100 !important; display: flex; align-items: center; gap: 10px;">
-          <span class="icon" style="font-size: 1.5rem; color: #0E3B4F;">✉️</span>
-          <span>contact@rumahbumnsemarang</span>
-        </li>
-        <li style="font-weight: 100 !important; display: flex; align-items: center; gap: 10px;">
-          <span class="icon" style="font-size: 1.5rem; color: #0E3B4F;">📍</span>
-          <span>Jl. Sultan Agung No.108, Wonotingal, Kec. Candisari,<br>Kota Semarang, Jawa Tengah 50232</span>
-        </li>
-        <li style="font-weight: 100 !important; display: flex; align-items: center; gap: 10px;">
-          <span class="icon" style="font-size: 1.5rem; color: #0E3B4F;">⏰</span>
-          <span>Senin - Jumat 08.00 - 16.00</span>
-        </li>
-      </ul>
-    </div>
-=======
   <div class="hubungi-page">
     <header class="hubungi-header">
       <div class="container">
         <h1 class="animate-fade-in">HUBUNGI KAMI</h1>
       </div>
     </header>
->>>>>>> 3c98c7c4fb871f4b07efb65642fa632fc5d6fb66
 
     <!-- Content Section -->
     <div class="hubungi-content">
