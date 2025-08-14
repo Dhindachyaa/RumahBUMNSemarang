57a0bc4f7ee90448a0f96fdf117c6644809327c6
Dhindachyaa <dindaramadani71@gmail.com>
2025-08-14 19:12:43 +0700

    final

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
 server: {
   proxy: {
     '/api': {
-      target: 'http://localhost:3000', // ✅ ganti ke port backend yang benar
+      target: 'http://localhost:3000', 
       changeOrigin: true,
       rewrite: path => path.replace(/^\/api/, '/api')
     }
